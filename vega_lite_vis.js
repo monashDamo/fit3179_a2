ec6e90c2cf363aa32e7b73cd1a250906dcfef0e5
monashDamo <dsin0019@student.monash.edu>
2024-10-16 23:48:15 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,44 @@
+var vg_1 = "graph1.vg.json";
+
+vegaEmbed("#powerStationsMap", vg_1).then(function(result) {
+
+}).catch(console.error)
+
+var vg_2 = "graph2.vg.json";
+
+vegaEmbed("#PowerPlantsCloseData", vg_2).then(function(result) {
+
+}).catch(console.error)
+
+var vg_3 = "energyProduction.vg.json";
+
+vegaEmbed("#energyPieChart", vg_3).then(function(result) {
+
+}).catch(console.error)
+
+var vg_4 = "allPlantsScattered.vg.json";
+
+vegaEmbed("#powerPlantBoxPlot", vg_4).then(function(result) {
+
+}).catch(console.error)
+
+var vg_5 = "energyUsageIndustries.vg.json";
+
+vegaEmbed("#industriesUsage", vg_5).then(function(result) {
+
+}).catch(console.error)
+
+var vg_6 = "energyExports.vg.json";
+
+vegaEmbed("#energyExports", vg_6).then(function(result) {
+
+}).catch(console.error)
+
+var vg_7 = "elecGenByYear.vg.json";
+
+vegaEmbed("#elecGenByYear", vg_7).then(function(result) {
+
+}).catch(console.error)
+
+
+
